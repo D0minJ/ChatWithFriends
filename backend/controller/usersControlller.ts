13e1a30633c04ee276ccee5db0171dd2818ef4b1
D0minJ <100507889+D0minJ@users.noreply.github.com>
2023-03-15 18:27:03 +0100

    Backend: Fix bugs

--- backend/controller/usersControlller.ts
+++ backend/controller/usersControlller.ts
@@ -3,19 +3,46 @@ import statusCode from "http-status-codes";
 import jwt from "jsonwebtoken"
 import User from "../Models/User";
 
+interface JwtPayload {
+    secureID: string
+}
+
+
 // Get list of users 
 const getUsers = async (req: Request, res: Response) => {
     const usersList: any = [];
     const value = req.params.name;
+    const cookies = req.cookies;
+    const refreshToken = cookies.rtoken;
+
+    if(!refreshToken){
+        return res.sendStatus(statusCode.UNAUTHORIZED);
+    }
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET!);
+    
+    const user = await User.findOne({secureID: (decoded as JwtPayload).secureID});
+
+    if(!user){
+        return res.sendStatus(statusCode.FORBIDDEN);
+    }
+
     
     try{
         const users = await User.find(
             {
-                $or: 
-                [
-                    {firstname: {$regex: "^" + value, $options: 'i'}},
-                    {lastname: {$regex: "^" + value, $options: 'i'}}
+                $and: [
+                    {
+                        $or: 
+                        [
+                            {firstname: {$regex: "^" + value, $options: 'i'}},
+                            {lastname: {$regex: "^" + value, $options: 'i'}}
+                        ]
+                    },
+                    {
+                        userID: { $nin: [user.userID]}
+                    }
                 ]
+                
             });
         users.map((user) => {
             usersList.push({userID: user.userID, firstname: user.firstname, lastname: user.lastname});
